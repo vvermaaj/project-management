7b4b394d4c29485b3e88a6edcc2f03d8f6688973
Ajay Kumar <145866309+vvermaaj@users.noreply.github.com>
2025-03-09 21:12:40 +0530

    Add files via upload

---
+++ projectmanagementsys/src/main/java/com.azash/request/CreateMessageRequest.java
@@ -0,0 +1,13 @@
+package com.azash.request;
+
+import lombok.Data;
+
+@Data
+public class CreateMessageRequest {
+        private Long senderId;
+        private String content;
+        private Long projectId;
+
+}
+
+
