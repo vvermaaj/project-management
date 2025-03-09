62ff83d2ed8bccd39137610a37c159d4b560798f
Ajay Kumar <145866309+vvermaaj@users.noreply.github.com>
2025-03-09 20:33:35 +0530

    Add files via upload

---
+++ React-frontend/project-management-system/src/pages/Login.jsx
@@ -0,0 +1,82 @@
+import React from 'react'
+import { useForm } from "react-hook-form";
+import { Button } from "@/components/ui/button";
+import { Form, FormField, FormItem, FormControl } from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { FormMessage } from "@/components/ui/form";
+import { useDispatch } from 'react-redux';
+import { login } from '@/Redux/Auth/Action';
+
+const Login = () => {
+    const dispatch=useDispatch()
+    const form = useForm({
+        //resolver:zod
+        defaultValues: {
+            email: "",
+            password: "",
+
+        },
+    });
+
+    const onSubmit = (data) => {
+        dispatch(login(data))
+        console.log("login project data", data);
+    };
+
+    return (
+        <div className="space-y-5">
+            <h1>Register</h1>
+            <Form {...form}>
+                <form className="space-y-5" onSubmit={form.handleSubmit(onSubmit)}>
+
+                    <FormField
+                        control={form.control}
+                        name="email"
+                        render={({ field }) => (
+                            <FormItem>
+                                <FormControl>
+                                    <Input
+                                        {...field}
+                                        type="text"
+                                        className="border w-full border-gray-700 py-5 px-5"
+                                        placeholder="email..."
+                                    />
+                                </FormControl>
+                                <FormMessage />
+                            </FormItem>
+                        )}
+                    />
+
+                    <FormField
+                        control={form.control}
+                        name="password"
+                        render={({ field }) => (
+                            <FormItem>
+                                <FormControl>
+                                    <Input
+                                        {...field}
+                                        type="text"
+                                        className="border w-full border-gray-700 py-5 px-5"
+                                        placeholder="password..."
+                                    />
+                                </FormControl>
+                                <FormMessage />
+                            </FormItem>
+                        )}
+                    />
+
+                    <Button
+                        type="submit"
+                        className="w-full mt-5">
+                        Login
+                    </Button>
+
+                </form>
+            </Form>
+
+
+        </div>
+    )
+}
+
+export default Login
