8816a998a448ab5c3f941e2b99adb59af9b13b56
Ajay Kumar <145866309+vvermaaj@users.noreply.github.com>
2025-03-09 20:35:57 +0530

    Create Home.jsx

---
+++ React-frontend/project-management-system/src/pages/Home/Home.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import ProjectList from "../ProjectList/ProjectList";
+
+const Home = () => {
+
+    return (
+        <div>
+        <ProjectList/>
+        </div>
+    );
+};
+
+export default Home;
