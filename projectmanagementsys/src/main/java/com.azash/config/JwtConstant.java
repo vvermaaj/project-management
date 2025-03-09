3f43a99644a7da91017565a1bb57e3ea6d32ff34
Ajay Kumar <145866309+vvermaaj@users.noreply.github.com>
2025-03-09 21:06:08 +0530

    Add files via upload

---
+++ projectmanagementsys/src/main/java/com.azash/config/JwtConstant.java
@@ -0,0 +1,8 @@
+package com.azash.config;
+
+public class JwtConstant {
+
+    public static final String SECRET_KEY="kjanfjkdnvkjndhvbsfhivbfshicbisdnbcisdcis akjancdjcdajcnjdncddcn hhdanckhjckjdncd hkjnckhnsdkc";
+    public static final String JWT_HEADER="Authorization";
+
+}
---
+++ projectmanagementsys/src/main/java/com.azash/config/JwtProvider.java
@@ -0,0 +1,36 @@
+package com.azash.config;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.security.core.Authentication;
+
+import javax.crypto.SecretKey;
+
+import java.util.Date;
+
+
+public class JwtProvider {
+
+    static SecretKey key = Keys.hmacShaKeyFor(JwtConstant.SECRET_KEY.getBytes());
+
+    public static String generateToken(Authentication auth){
+
+        return Jwts.builder().setIssuedAt(new Date())
+                .setExpiration(new Date(new Date().getTime()+86400000))
+                .claim("email", auth.getName())
+                .signWith(key)
+                .compact();
+
+    }
+    public static String getEmailFromToken(String jwt){
+        //Bearer token
+jwt=jwt.substring(7);
+            Claims claims = Jwts.parserBuilder().setSigningKey(key).build().parseClaimsJws(jwt).getBody();
+
+        return String.valueOf(claims.get("email"));
+
+
+    }
+
+}
