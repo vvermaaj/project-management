b12ce387864075698198146be54f0b30a884071d
Ajay Kumar <145866309+vvermaaj@users.noreply.github.com>
2025-03-09 21:10:44 +0530

    Add files via upload

---
+++ projectmanagementsys/src/main/java/com.azash/repository/InviteRequest.java
@@ -0,0 +1,14 @@
+package com.azash.repository;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class InviteRequest {
+    private Long projectId;
+    private String email;
+
+}
---
+++ projectmanagementsys/src/main/java/com.azash/repository/MessageRepository.java
@@ -0,0 +1,14 @@
+package com.azash.repository;
+
+import com.azash.model.Message;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface MessageRepository extends JpaRepository <Message, Long> {
+
+
+    List<Message> findByChatIdOrderByCreatedAtAsc(Long chatId);
+
+
+}
