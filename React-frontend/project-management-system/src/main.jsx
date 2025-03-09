7afbaf3f37daf0407a026a0d8d52c99830f04778
Ajay Kumar <145866309+vvermaaj@users.noreply.github.com>
2025-03-09 20:51:31 +0530

    Add files via upload

---
+++ React-frontend/project-management-system/src/main.jsx
@@ -0,0 +1,21 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'
+import { BrowserRouter } from 'react-router-dom'
+import React from 'react'
+import { Provider } from 'react-redux'
+import { store } from './Redux/Store.js'
+
+
+createRoot(document.getElementById('root')).render(
+ <StrictMode>
+    <BrowserRouter>
+    <Provider store={store}>
+    <App />
+    </Provider>
+    
+    </BrowserRouter>
+    
+ </StrictMode>,
+)
