4b8a88d96e3042e20d9cfa1a7bb9d0d13a1720da
Ajay Kumar <145866309+vvermaaj@users.noreply.github.com>
2025-03-09 21:16:05 +0530

    Add files via upload

---
+++ projectmanagementsys/src/main/java/com.azash/ProjectManagementSysApplication.java
@@ -0,0 +1,13 @@
+package com.azash;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectManagementSysApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectManagementSysApplication.class, args);
+	}
+
+}
